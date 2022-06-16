016e754f0872125778b4b1ba3c553dcbb348f442
jacocho858 <jacocho858@gmail.com>
2022-06-16 16:32:31 +0900

    board restAPI 연습

---
+++ testvue/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+
+const app = createApp(App).use(router);
+app.config.globalProperties.axios = axios;
+app.mount('#app');
